bed31a0e2ee8bf02f9fbe717f8f3afe57af301ef
rtakeshi91 <rtakeshi91@gmail.com>
2025-02-22 13:10:15 -0300

    commit inicial

---
+++ src/test/java/com/itau/pj/recuperacao/cobranca/CobrancaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itau.pj.recuperacao.cobranca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CobrancaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
